ca424aad02426ddb0675c261da4a385c73840ea1
marimontefusco <mari.montefusco@gmail.com>
2024-05-03 16:02:24 -0300

    inicial repo - gradle config

---
+++ app/src/main/java/com/agenda/agendaproject/model/Usuario.kt
@@ -0,0 +1,20 @@
+package com.agenda.agendaproject.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tb_usuarios")
+data class Usuario (
+    @ColumnInfo(name= "nome") val nome: String?, //? -> aceita null
+    @ColumnInfo(name = "sobrenome") val sobrenome: String?,
+    @ColumnInfo(name = "idade") val idade: String,
+    @ColumnInfo(name = "celular") val celular: String?,
+    @ColumnInfo(name = "email") val email: String?
+) {
+    @PrimaryKey(autoGenerate = true)
+    val uid: Int = 0 // gera o id automaticamente -> dinâmico
+}
+
+// Entidade -> representa a tabela
+// é uma data class
